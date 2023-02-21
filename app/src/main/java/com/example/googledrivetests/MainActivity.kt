34f3e65d9520e2d63149a1f902af02450b43831c
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2023-02-20 23:53:00 -0600

    adding all functionality we need for google drive
    adding all functionality we need for google drive

--- app/src/main/java/com/example/googledrivetests/MainActivity.kt
+++ app/src/main/java/com/example/googledrivetests/MainActivity.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.drive.DriveApi
+import com.google.api.client.http.FileContent
 import com.google.api.client.http.HttpRequestInitializer
 import com.google.api.client.http.HttpTransport
 import com.google.api.client.json.JsonFactory
@@ -19,6 +20,7 @@ import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.model.File
 import com.google.api.services.drive.model.FileList
+import com.google.api.services.drive.model.Permission
 import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.GoogleCredentials
 import kotlinx.coroutines.CoroutineScope
@@ -58,18 +60,20 @@ class MainActivity : AppCompatActivity() {
             .build()
         Log.e("Storage:","$storage")
 
-        val file = xFile(this.cacheDir, "for-phil-tests-84356-83ccab46dc55.json")
-        val filePath = file.path
+//        val file = xFile(this.cacheDir, "for-phil-tests-84356-83ccab46dc55.json")
+//        val filePath = file.path
         CoroutineScope(Dispatchers.IO).launch {
             runBlocking {
                 val listOfDriveFiles = storage.Files().list()
                     .execute()
                 Log.e(
                     "file path and list of drive files",
-                    "$filePath drive files = $listOfDriveFiles  size = ${listOfDriveFiles.size}"
+                    "xxpathHere drive files = $listOfDriveFiles  size = ${listOfDriveFiles.size}"
                 )
             }
         }
+
+        //TODO create a folder with specific name
 
 //        val metaDataSet = MetadataChangeSet.Builder()
 //            .setTitle("firstTestFolderRoot")
@@ -105,34 +109,89 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
+        val folder_id = "1wwvZ8j20rPBqCK5JphMNt-HBcSHvS94Y"
         CoroutineScope(Dispatchers.IO).launch {
             runBlocking {
                 val files = mutableListOf<File>()
 
+                val folderName = "firstTestFolderRoot"  //TODO this is the name we are searching fore , could be a user, trainee etc..
                 var pageToken: String? = null
                 do {
+                //TODO searching by folder name (which can be the trainee name or id and that way we get the actual folder id on the result)
                     val result: FileList = storage.files().list()
-                        .setQ("fullText contains '\"hello world\"'")
+                        .setQ("mimeType='application/vnd.google-apps.folder' and name='$folderName' and trashed=false")//.setQ("'$folder_id' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false")
                         .setSpaces("drive")
-                        .setFields("nextPageToken, items(id, title)")
+                        .setFields("nextPageToken, files(id,name,mimeType)")
                         .setPageToken(pageToken)
                         .execute()
-                    for (file in result.files) {
-                        System.out.printf(
-                            "Found file: %s (%s)\n",
-                            file.name, file.id
-                        )
+                    for (f in result.files) {
+                        files.add(f)
                     }
-
-                    files.addAll(result.files)
+                    Log.e("list result = ", "$files")
                     pageToken = result.nextPageToken
+                    Log.e("page token = ","$pageToken")
                 } while (pageToken != null)
 
-                Log.e("list result = ", "$files")
-            }
-        }
+                //TODO get files in specific folder
+                val q = "'$folder_id' in parents and trashed = false"
+                val filesInSpecificFolderByID = storage.files().list()
+                    .setQ(q)
+                    .execute().files
+                Log.e("Files in specific folder by ID","$filesInSpecificFolderByID")
 
+                //TODO upload file to specific folder using the folder id
+                val fileMetadata = File()
+                fileMetadata.name = "forPhilJson.json"
+
+                //TODO Set the parents parameter to the ID of the folder you want to upload the image to
+//                val parents = mutableListOf<String>()
+//                parents.add(folder_id)
+//                fileMetadata.parents = parents
+//
+//                val actualFile = xFile(this@MainActivity.cacheDir, "for-phil-tests-84356-83ccab46dc55.json")
+//                val filePath = actualFile.path
+//                val fileToUpload = xFile(filePath)
+//                val mediaContent = FileContent("application/json",fileToUpload)
+//
+//                val file = storage.files().create(fileMetadata, mediaContent)
+//                    .setFields("id, name, parents")
+//                    .execute()
+            }
+            //TODO delete files or folders
+            //storage.files().delete("1wwvZ8j20rPBqCK5JphMNt-HBcSHvS94Y").execute()
+
+            val emailAddressToShareTo = "lionprodev@gmail.com" //TODO share folder so that they have a way to see the folders and contents
+                                                                 //TODO created by the app throughout use on a regular google drive account with GUI
+//            val permission = Permission()
+//                .setType("user")
+//                .setRole("writer")
+//                .setEmailAddress(emailAddressToShareTo)
+//
+//            storage.permissions().create(folder_id,permission).execute()
+
+            //TODO delete permission to share specific folder (by id ) to an specific email address
+            // Get the list of permissions for the folder
+//            val permissions = storage.permissions().list(folder_id).execute().permissions
+//
+//            var permissionId: String? = null
+//            for (permission in permissions) {
+//                Log.e("Permission email in for loop","$permission")
+//                if (permission.emailAddress == emailAddressToShareTo) {
+//                    permissionId = permission.id
+//                    break
+//                }
+//            }
+//
+//            permissionId = "12341993584222948980"
+//            if (permissionId != null) {
+//                storage.permissions().delete(folder_id, permissionId).execute()
+//                Log.e("Permission for $emailAddressToShareTo", "removed from folder $folder_id")
+//
+//            }else{
+//                Log.e("permision ID is NULL","$permissionId")
+//            }
+
+        }
 
     }
 
